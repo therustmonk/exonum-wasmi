7f6e6aaf4c176e8f84f744cd41c03c6fd8f95b34
Denis Kolodin <denis.kolodin@xdev.re>
2018-02-08 13:48:33 +0300

    Implement transfer

--- cryptocurrency/src/lib.rs
+++ cryptocurrency/src/lib.rs
@@ -19,15 +19,39 @@ impl Wallet {
     }
 }
 
+impl Wallet {
+    pub fn increase(self, amount: u64) -> Self {
+        let balance = self.balance + amount;
+        Wallet { balance }
+    }
+
+    pub fn decrease(self, amount: u64) -> Self {
+        let balance = self.balance - amount;
+        Wallet { balance }
+    }
+}
 
 #[derive(Deserialize)]
 struct Transfer {
     to: Sender,
+    amount: u64,
 }
 
 #[no_mangle]
 pub fn transfer_funds() {
     let args: Transfer = ext::args();
+    let sender = ext::sender();
+    let from: Option<Wallet> = ext::get_storage(&sender);
+    let to: Option<Wallet> = ext::get_storage(&args.to);
+    if let (Some(from), Some(to)) = (from, to) {
+        if from.balance >= args.amount {
+            let from = from.decrease(args.amount);
+            let to = to.increase(args.amount);
+            ext::set_storage(&sender, &from);
+            ext::set_storage(&args.to, &to);
+            ext::debug(format!("transfered <{}> from {:?} to {:?}", &args.amount, sender.0, &args.to.0).as_bytes());
+        }
+    }
 }
 
 #[no_mangle]
